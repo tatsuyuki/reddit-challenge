632a40c4a32906729101a793a25500982e68fe62
tatsuyuki <michael84@gmail.com>
2018-03-28 16:20:00 +0800

    completed cipher logic

--- alphabetcipher355/src/alphabetcipher355/Main.java
+++ alphabetcipher355/src/alphabetcipher355/Main.java
@@ -3,13 +3,18 @@ import java.util.Scanner;
 
 
 public class Main {
-
+	
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String n = "";
-		String m = "";
+		Scanner reader = new Scanner(System.in);
+		System.out.println("Enter keyword");
+		char[] n = reader.nextLine().toCharArray();
+		System.out.println("Enter phrase");
+		char[] m = reader.nextLine().toCharArray();
 		
-		alphaCipher alphaC = new alphaCipher(n, m);		
+		alphaCipher alphaC = new alphaCipher(n, m);	
+		
 
 	}
 	
--- alphabetcipher355/src/alphabetcipher355/alphaCipher.java
+++ alphabetcipher355/src/alphabetcipher355/alphaCipher.java
@@ -5,14 +5,42 @@ import java.util.Map;
 
 public class alphaCipher {
 	
-	private String key;
-	private String message;
+	private char[] key;
+	private char[] message;
 	private static char[] alphabet = "abcdefghijklmnopqrstuvwxyz".toCharArray();
 	private static Map<Character, Integer>cipher = new HashMap<Character, Integer>();
 	
-	public alphaCipher(String key, String message) {
-		char a = alphabet[0];
-		System.out.print(a);
+	public alphaCipher(char[] key, char[] message) {
+		//char a = alphabet[0];
+		//System.out.print(a);
+		this.key = key;
+		this.message = message;
+		for (int i = 0; i < alphabet.length; i++) {
+			cipher.put(alphabet[i],i);
+		}
+		System.out.println("Encryption: " + encrypt(key, message));
+		char[]encryptedMsg = encrypt(key,message).toCharArray();
+		System.out.println("Decryption: " + decrypt(key,encryptedMsg));
+		
+	}
+	
+	private static String encrypt(char[] phrase, char[] msg) {
+		StringBuilder sb = new StringBuilder();
+		for(int i = 0, j = 0; i < msg.length; i++, j = (j + 1) % phrase.length) {
+			sb.append(alphabet[(cipher.get(msg[i]) + cipher.get(phrase[j])) % alphabet.length]);
+			
+		}
+		return sb.toString();
+
+	}
+	
+	private static String decrypt(char[] phrase, char[] msg) {
+		StringBuilder sb = new StringBuilder();
+		for(int i = 0, j = 0; i < msg.length; i++, j = (j + 1) % phrase.length) {
+			sb.append(alphabet[(cipher.get(msg[i]) - cipher.get(phrase[j]) + alphabet.length) % alphabet.length]);
+			
+		}
+		return sb.toString();
 	}
 
 }
